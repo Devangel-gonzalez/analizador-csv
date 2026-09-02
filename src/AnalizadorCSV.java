82abcb34dd95e523271950c17c2216ab0e406c21
Angel Gonzalez <a222213581@unison.mx>
2026-09-02 09:32:37 -0700

    Configuracion.java

--- src/AnalizadorCSV.java
+++ src/AnalizadorCSV.java
@@ -6,6 +6,23 @@ public class AnalizadorCSV {
 
     public static void main(String[] args) {
 
+        Configuracion configuracion = new Configuracion(args);
+
+        System.out.println(
+                "Configuración de la aplicación");
+
+        System.out.println(
+                "Archivo: "
+                        + configuracion.getArchivo());
+
+        System.out.println(
+                "Separador: "
+                        + configuracion.getSeparador());
+
+        System.out.println(
+                "Salida: "
+                        + configuracion.getDirectorioSalida());
+
         // Cargar archivo de propiedades
         Properties propiedades = new Properties();
         try (
